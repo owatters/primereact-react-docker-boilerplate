0c86b16bf0b6c7b498494689bd7fa29bb9dd3d62
Amit Gupta <amit@owatters.com>
2019-02-07 16:31:59 +0530

    added support for language files

--- FE/src/containers/sidebar/sidebar.js
+++ FE/src/containers/sidebar/sidebar.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {Sidebar} from "primereact/sidebar";
 import {Button} from "primereact/button";
 import SidebarItem from "../../components/sidebaritem/sidebaritem.js";
+import en from "../../en.json";
 
 export default class SideBar extends Component {
     constructor() {
@@ -15,7 +16,8 @@ export default class SideBar extends Component {
         return(
             <div>
             <Sidebar visible={this.state.visible} onHide={(e) => this.setState({visible:false})}>
-           <SidebarItem  label="Home"></SidebarItem>
+           <SidebarItem  label={en.HOME}></SidebarItem>
+           <SidebarItem  label={en.SETTINGS}></SidebarItem>
         </Sidebar>
         
         <Button icon="pi pi-arrow-right" onClick={(e) => this.setState({visible:true})}/>
